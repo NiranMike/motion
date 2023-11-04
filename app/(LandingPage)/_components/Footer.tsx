7b4242965cffb78444069e7780c06c7bedd8e2cb
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-11-04 11:43:22 +0100

    change text color

--- app/(LandingPage)/_components/Footer.tsx
+++ app/(LandingPage)/_components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='flex bg-blue-400 dark:bg-[#1f1f1f] items-center w-full p-6 bg-background z-50'>
         <Logo />
-        <div className='md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground'>
+        <div className='md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-black md:text-muted-foreground'>
             <Button size={"sm"} variant={"ghost"}>Privacy Policy</Button>
             <Button size={"sm"} variant={"ghost"}>Terms & Conditions</Button>
         </div>
